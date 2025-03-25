27e221b2b7d16513245d99cac49d8c17adee6bbd
Bunny Studio <rudradalal0082004@gmail.com>
2025-03-25 07:38:57 +0530

    Update product.js

--- js/product.js
+++ js/product.js
@@ -28,4 +28,14 @@ document.addEventListener("DOMContentLoaded", function () {
       alert("Item added to cart successfully!");
     });
   });
+var menuitems = document.getElementById("menuitems");
+menuitems.style.maxHeight = "0px";
+
+function menutoggle() {
+  if (menuitems.style.maxHeight == "0px") {
+    menuitems.style.maxHeight = "200px";
+  } else {
+    menuitems.style.maxHeight = "0px";
+  }
+}
 });
